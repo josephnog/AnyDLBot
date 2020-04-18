e8aeda96c338a0fb099bbdc46be624e8769d31ad
justinthoms <57831354+justinthoms@users.noreply.github.com>
2020-04-18 09:57:25 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """This is an @AnyDLBot clone created using the Open Source code available.
+    START_TEXT = """This is an @AnyDLBot cloneing tcode available.
 Source Code: https://github.com/SpEcHiDe/AnyDLBot
 © @AnyDLBot"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
